6082912bff18c0703fd501a74ff9b7d27d8904b1
Jan-Hendrik Matthes <matthesjh@gmail.com>
2019-11-17 01:12:23 +0100

    Complete the documentation comments of the performMove method [skip ci]

--- Sources/simple-client/SCGameState.swift
+++ Sources/simple-client/SCGameState.swift
@@ -153,6 +153,8 @@ class SCGameState: CustomStringConvertible {
 
     /// Performs the given move on the game board.
     ///
+    /// - Parameter move: The move to be performed.
+    ///
     /// - Returns: `true` if the move could be performed; otherwise, `false`.
     func performMove(move: SCMove) -> Bool {
         // TODO: Validate the move.
